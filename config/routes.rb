510c5a139bdd9564c000fe947e75a53fab3df0de
Ben Benedict <Ben.Benedict@gmail.com>
2021-04-10 20:36:28 -0500

    made sessions route there gonna be conflicts in routes file like i said

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  #there are going to be conflicts here like i said so i'm going to be annoyed
   namespace :api do
     post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
   end
 end
